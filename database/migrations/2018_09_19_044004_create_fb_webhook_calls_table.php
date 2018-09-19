c4302d4930d3758bcc91a22b1afff749794547a0
vinit <vinit@barten.biz>
2018-09-19 10:26:19 +0530

    add migration tables

---
+++ database/migrations/2018_09_19_044004_create_fb_webhook_calls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFbWebhookCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fb_webhook_calls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands');;
+            $table->string('user_ref');
+            $table->json('payload');
+            $table->boolean('message_sent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fb_webhook_calls');
+    }
+}
