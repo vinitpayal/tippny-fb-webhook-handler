29069a89a01f2c1ae53e34b5dab98a01689417b2
vinitpayal <vinitpayal@gmail.com>
2018-09-26 10:52:27 +0530

    check for eligible messages every seconds

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('message:cart-product')
-                  ->everyMinute();
+             ->cron('* * * * * *')->withoutOverlapping();
     }
 
     /**
