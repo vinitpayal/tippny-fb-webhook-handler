1b4ca725dccc91cf6fe4ff94b0ea083e86f7afe9
vinitpayal <vinitpayal@gmail.com>
2018-10-15 10:49:22 +0530

    class name change

--- app/Model/WelcomeMessageSentDump.php
+++ app/Model/WelcomeMessageSentDump.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Brand extends Model
+class WelcomeMessageSentDump extends Model
 {
     protected $table = 'welcome_messages';
 
